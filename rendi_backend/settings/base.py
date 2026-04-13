5c841164cbab1f804be428d6181d3fe9f1c9ce2c
oluwaseyipd <oluwaseyipd@gmail.com>
2026-04-13 18:47:24 +0100

    feat: add PyMySQL setup for MySQLdb integration in Django

--- rendi_backend/settings/base.py
+++ rendi_backend/settings/base.py
@@ -9,6 +9,14 @@ from pathlib import Path
 from datetime import timedelta
 from decouple import config
 
+# -----------------------------------------------------------------
+#  PyMySQL setup to allow using MySQL with Django's MySQLdb backend
+# -----------------------------------------------------------------
+import pymysql
+pymysql.version_info = (2, 2, 1, "final", 0) # Mock the version Django wants
+pymysql.install_as_MySQLdb()
+
+
 BASE_DIR = Path(__file__).resolve().parent.parent.parent
 
 # ------------------------------------------------------------------
